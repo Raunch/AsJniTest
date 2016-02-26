2bf3cc214e1218f82b2d54bef22915c1a1e97be5
Raunch <zhangsongshun@163.com>
2016-02-26 16:13:40 +0800

    增加JNI对应的Java文件

--- app/src/main/java/com/raunch/asjnitest/MineJniTest.java
+++ app/src/main/java/com/raunch/asjnitest/MineJniTest.java
@@ -4,6 +4,9 @@ package com.raunch.asjnitest;
  * Created by songshunzhang on 16-2-26.
  */
 public class MineJniTest {
+    static {
+        System.loadLibrary("");
+    }
     public native String getCLanguageString();
 
 }
